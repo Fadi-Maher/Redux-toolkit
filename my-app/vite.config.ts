7dcf70d4158b54ce2507f6eba396e03814245203
Fady Maher <cleverfady@gmail.com>
2025-09-02 14:04:23 +0300

    handle vite config

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -2,9 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,  tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // يضمن إن Vercel يعرف فين ملفات الـ build
+  },
+  server: {
+    port: 5173, // للـ dev بس
+  },
 })
+
+
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react() ,  tailwindcss()
+//   ],
+// })
