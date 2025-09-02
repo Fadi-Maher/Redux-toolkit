fc134f65cec5eb0ab78aaff4df2a5afe7990ae4d
Fady Maher <cleverfady@gmail.com>
2025-09-02 14:19:30 +0300

    handle vercel.json

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist", // يضمن إن Vercel يعرف فين ملفات الـ build
-  },
-  server: {
-    port: 5173, // للـ dev بس
-  },
+    outDir: "dist"
+  }
 })
 
 
